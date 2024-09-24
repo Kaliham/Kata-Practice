2eafd14461c98eee594e8932ce18b5e995c02848
Kaliham <theKaliham@gmail.com>
2024-09-24 18:23:01 -0400

    fixed rune reading issue and reading return carrage and line feed

--- Go/ex1.go
+++ Go/ex1.go
@@ -4,35 +4,51 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 )
 
 func main() {
 	var input string
 	var err error
 	var arr []string
-	fmt.Println("Enter String to split")
+
 	reader := bufio.NewReader(os.Stdin)
-	input, err = reader.ReadString('\n')
+	fmt.Println("How Many ")
+	fmt.Println("Enter String to split")
+	input, err = reader.ReadString(10)
 	if err != nil {
 		fmt.Println("Error Occured while reading input")
 	}
+	//removing the return carrage and line feed
+	input = strings.TrimRightFunc(input, func(r rune) bool { return r <= 13 })
 	arr = split(input)
 
-	fmt.Println(arr)
+	fmt.Print(arr)
 }
 
 func split(str string) (arr []string) {
-	a := []rune(str)
+	a := make([]string, len(str))
+	for i, r := range str {
+		a[i] = string(r)
+	}
+
 	l := len(a)
 	s := l/2 + l%2
-	arr = make([]string, s)
+	t := []string{""}
+	arr = slices.Repeat(t, s)
 	var i int
-	var c rune
+	var c string
+	fmt.Println(a)
 	for i, c = range a {
-		arr[i/2] += string(c)
+		arr[i/2] = arr[i/2] + string(c)
 	}
 	if l%2 == 1 {
 		arr[i/2] += "_"
+	}
+
+	for _, e := range arr {
+		fmt.Printf("%v,", e)
 	}
 	return
 }
